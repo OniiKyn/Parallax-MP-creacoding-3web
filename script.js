02114295ca7ac8abe58ef8e43c25c473b861d588
OniiKyn <megann.p@live.fr>
2025-02-28 21:56:17 +0100

    Update final
    Update HTML/CSS
    tentative de Parallax / js qui ne fonctionne pas et me casse tout le code :)

---
+++ script.js
@@ -0,0 +1,7 @@
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
+
+//You can also choose to apply the parallax on multiple images:
+
+var images = document.querySelectorAll('img');
+new simpleParallax(images);
